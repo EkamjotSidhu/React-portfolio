4433d9dc0ea0e88df1f2be7b898d046c82a10982
EkamjotSidhu <ekamsidhu5678@gmail.com>
2023-02-24 20:28:04 -0500

    Updated portfolio

--- src/component/pages/portfolio.js
+++ src/component/pages/portfolio.js
@@ -2,61 +2,98 @@ import React from "react";
 import miniGames from ".././Assets/Images/mini games.png"
 import github from ".././Assets/Images/github.png"
 import web from ".././Assets/Images/web.png"
-import notemaker from".././Assets/Images/notemaker.png"
-import custompcbuild from".././Assets/Images/cutompcbuild.png"
-import weatherdashboard from".././Assets/Images/weatherdashboard.png"
+import notemaker from ".././Assets/Images/notemaker.png"
+import custompcbuild from ".././Assets/Images/cutompcbuild.png"
+import weatherdashboard from ".././Assets/Images/weatherdashboard.png"
 import quizchallenge from ".././Assets/Images/quizchallenge.png"
 import workdayscheduler from ".././Assets/Images/workdayscheduler.png"
-
+import eCommerce from "../Assets/Images/collections.jpg"
 function Portfolio() {
     const portfolio = [{
         "image": miniGames,
         "title": "Mini games",
-        "github": {"icon":github,
-            "link":"https://github.com/Jon-Ledo/Collab-MiniGames-plusAPI"},
-        "deployedLink": {"icon":web,
-            "link":"https://jon-ledo.github.io/Collab-MiniGames-plusAPI/"}
+        "github": {
+            "icon": github,
+            "link": "https://github.com/Jon-Ledo/Collab-MiniGames-plusAPI"
+        },
+        "deployedLink": {
+            "icon": web,
+            "link": "https://jon-ledo.github.io/Collab-MiniGames-plusAPI/"
+        }
     },
     {
         "image": custompcbuild,
         "title": "Custom Pc Build",
-        "github": {"icon":github,
-        "link":"https://github.com/DustinLee0/Project-2-Custom-PC-Build"},
-    "deployedLink": {"icon":web,
-        "link":"https://custom-pc.herokuapp.com/"}
+        "github": {
+            "icon": github,
+            "link": "https://github.com/DustinLee0/Project-2-Custom-PC-Build"
+        },
+        "deployedLink": {
+            "icon": web,
+            "link": "https://custom-pc.herokuapp.com/"
+        }
     },
     {
         "image": notemaker,
         "title": "Note Maker",
-        "github": {"icon":github,
-            "link":"https://github.com/EkamjotSidhu/Note-taking-app"},
-        "deployedLink": {"icon":web,
-            "link":"https://note-app-111.herokuapp.com/"}
+        "github": {
+            "icon": github,
+            "link": "https://github.com/EkamjotSidhu/Note-taking-app"
+        },
+        "deployedLink": {
+            "icon": web,
+            "link": "https://note-app-111.herokuapp.com/"
+        }
     },
     {
         "image": weatherdashboard,
         "title": "Weather Dashboard",
-        "github": {"icon":github,
-            "link":"https://github.com/EkamjotSidhu/Weather-forecast-app"},
-        "deployedLink": {"icon":web,
-            "link":"https://ekamjotsidhu.github.io/Weather-forecast-app/"}
+        "github": {
+            "icon": github,
+            "link": "https://github.com/EkamjotSidhu/Weather-forecast-app"
+        },
+        "deployedLink": {
+            "icon": web,
+            "link": "https://ekamjotsidhu.github.io/Weather-forecast-app/"
+        }
     },
     {
         "image": quizchallenge,
         "title": "Quiz Challenge",
-        "github": {"icon":github,
-            "link":"https://github.com/EkamjotSidhu/Week-4-Challenge-Quiz"},
-        "deployedLink": {"icon":web,
-            "link":"https://ekamjotsidhu.github.io/Week-4-Challenge-Quiz/"}
+        "github": {
+            "icon": github,
+            "link": "https://github.com/EkamjotSidhu/Week-4-Challenge-Quiz"
+        },
+        "deployedLink": {
+            "icon": web,
+            "link": "https://ekamjotsidhu.github.io/Week-4-Challenge-Quiz/"
+        }
     },
     {
         "image": workdayscheduler,
         "title": "Work Day Scheduler",
-        "github": {"icon":github,
-        "link":"https://github.com/EkamjotSidhu/Workday-scheduler"},
-    "deployedLink": {"icon":web,
-        "link":"https://ekamjotsidhu.github.io/Workday-scheduler/"}
-    }]
+        "github": {
+            "icon": github,
+            "link": "https://github.com/EkamjotSidhu/Workday-scheduler"
+        },
+        "deployedLink": {
+            "icon": web,
+            "link": "https://ekamjotsidhu.github.io/Workday-scheduler/"
+        }
+    },
+    {
+        "image": eCommerce,
+        "title": "E Commerce",
+        "github": {
+            "icon": github,
+            "link": "https://github.com/EkamjotSidhu/project-3-E-commerce"
+        },
+        "deployedLink": {
+            "icon": web,
+            "link": "https://on-code-e-commerce.herokuapp.com/"
+        }
+    }
+    ]
 
 
     const pop = portfolio.map(activity => {
@@ -74,7 +111,7 @@ function Portfolio() {
                         <a href={activity.github.link}><img className="iconss" src={activity.github.icon}></img></a>
                     </div>
                     <div>
-                        <a href={activity.deployedLink.link}><img className="iconss"src={activity.deployedLink.icon}></img></a>
+                        <a href={activity.deployedLink.link}><img className="iconss" src={activity.deployedLink.icon}></img></a>
                     </div>
                 </div>
             </div>
